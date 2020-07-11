490c51936dbbc420ded53be3f0ff9d1b24d78c4c
Yafei-Guo-coder <yafeiguo116@gmail.com>
2020-07-11 12:47:44 +0800

    Start

--- src/EditFile/MergeVCF.java
+++ src/EditFile/MergeVCF.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package src;
+package EditFile;
 
 import static com.google.common.io.Files.map;
 import static com.univocity.parsers.conversions.Conversions.string;
