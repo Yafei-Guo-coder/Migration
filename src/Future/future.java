493cbad741805f987c879b68b7e43d9564ab7533
Yafei-Guo-coder <yafeiguo116@gmail.com>
2020-07-11 12:51:43 +0800

    Create a new package

---
+++ src/Future/future.java
@@ -0,0 +1,13 @@
+package Future;
+
+import com.sun.org.apache.xml.internal.res.XMLErrorResources_tr;
+
+/**
+ * @author Yafei Guo
+ * @create 2020-07-11 12:49 PM
+ */
+public class future {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
