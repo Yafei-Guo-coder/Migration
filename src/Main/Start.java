01a273204cf4a00359c12a150eb267ca30fea206
Yafei Guo <yafeiguo116@gmail.com>
2021-11-12 20:18:37 +0800

    update codes

--- src/Main/Start.java
+++ src/Main/Start.java
@@ -17,13 +17,13 @@ import static Function.HaploType.getHaplo;
  */
 public class Start {
     public static void main(String[] args) throws IOException {
-//        String[] str = new String[42];
-//        String temp = null;
-//        BufferedReader lib = IOUtils.getTextReader("/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/lib_3.1.txt");
-//        for (int i = 0; i < 28; i++) {
-//            str[i] = lib.readLine();
-//        }
-//        int chrABD[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42};
+        String[] str = new String[42];
+        String temp = null;
+        BufferedReader lib = IOUtils.getTextReader("/Users/guoyafei/Documents/02_VmapIII/03_Fastcall2/lib_3.1.txt");
+        for (int i = 0; i < 42; i++) {
+            str[i] = lib.readLine();
+        }
+        int chrABD[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42};
 //        int chrAB[] = {1,2,3,4,7,8,9,10,13,14,15,16,19,20,21,22,25,26,27,28,31,32,33,34,37,38,39,40};
 //        int chrD[] = {5,6,11,12,17,18,23,24,29,30,35,36,41,42};
 
@@ -31,19 +31,19 @@ public class Start {
 //        Para1(chrAB,"/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/测试数据/fastCall/inputfile/Fastcall2_1_parameters.txt","/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/para1/","AB_taxaBamMap.txt");
 //        Para2(chrAB,"/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/测试数据/fastCall/inputfile/Fastcall2_2_parameters.txt","/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/para2/");
 //        Para3(chrAB,str,"/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/测试数据/fastCall/inputfile/Fastcall2_3_parameters.txt","/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/para3/","AB_taxaBamMap.txt");
-//        Para1(chrD,"/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/测试数据/fastCall/inputfile/Fastcall2_1_parameters.txt","/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/para1/","20D_taxaBamMap.txt");
-//        Para2(chrD,"/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/测试数据/fastCall/inputfile/Fastcall2_2_parameters.txt","/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/para2");
-//        Para3(chrD,str,"/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/测试数据/fastCall/inputfile/Fastcall2_3_parameters.txt","/Users/guoyafei/Documents/01_个人项目/04_VmapIII/09_Fastcall2/para3/","20D_taxaBamMap.txt");
+//        Para1(chrD,"/Users/guoyafei/Documents/02_VmapIII/03_Fastcall2/para1/1_parameters.txt","/Users/guoyafei/Documents/02_VmapIII/03_Fastcall2/para1/","D_taxaBamMap.txt");
+//        Para2(chrD,"/Users/guoyafei/Documents/02_VmapIII/03_Fastcall2/para2/1_parameters.txt","/Users/guoyafei/Documents/02_VmapIII/03_Fastcall2/para2/");
+        Para3(chrABD,str,"/Users/guoyafei/Documents/02_VmapIII/03_Fastcall2/测试数据/fastCall/inputfile/Fastcall2_3_parameters.txt","/Users/guoyafei/Documents/02_VmapIII/03_Fastcall2/para3/","D_taxaBamMap.txt");
 
+//
+//            List<File> Dirlist = IOUtils.getFileListInDir("/Users/guoyafei/Documents/01_Migration/02_Environment/02_XP-CLR/Gene/V5/VCF");
+//
+//            for (int j = 0; j < Dirlist.size(); j++) {
+//
+//                getHaplo(Dirlist.get(j).getAbsolutePath(),Dirlist.get(j).getAbsolutePath().replace("vcf","txt"));
+//            }
 
-            List<File> Dirlist = IOUtils.getFileListInDir("/Users/guoyafei/Documents/01_个人项目/02_Migration/02_数据表格/01_Vmap1-1/01_Add_ZNdata/05_Environment/XP-CLR/Gene/VIP_gene/VCF/Pos");
-
-            for (int j = 0; j < Dirlist.size(); j++) {
-
-                getHaplo(Dirlist.get(j).getAbsolutePath(),Dirlist.get(j).getAbsolutePath().replace("vcf","txt"));
-            }
-
-//        getHaplo("/Users/guoyafei/Documents/01_个人项目/02_Migration/02_数据表格/01_Vmap1-1/01_Add_ZNdata/05_Environment/XP-CLR/Gene/pos.vcf","/Users/guoyafei/Documents/01_个人项目/02_Migration/02_数据表格/01_Vmap1-1/01_Add_ZNdata/05_Environment/XP-CLR/Gene/pos.txt");
+//        getHaplo("/Users/guoyafei/Documents/02_VmapIII/04_Statistics/Q.recode.vcf","/Users/guoyafei/Documents/02_VmapIII/04_Statistics/Q.recode.txt");
 //        getHaplo("/Users/guoyafei/Documents/01_个人项目/05_FuGWAS/06_单倍型分析0419/01_VCF/geneA_mac.vcf","/Users/guoyafei/Documents/01_个人项目/05_FuGWAS/06_单倍型分析0419/02_haplo/geneA_haplo.txt");
 //        getHaplo("/Users/guoyafei/Documents/01_个人项目/05_FuGWAS/06_单倍型分析0419/01_VCF/geneB_mac.vcf","/Users/guoyafei/Documents/01_个人项目/05_FuGWAS/06_单倍型分析0419/02_haplo/geneB_haplo.txt");
 //        getHaplo("/Users/guoyafei/Documents/01_个人项目/05_FuGWAS/06_单倍型分析0419/01_VCF/geneD_mac.vcf","/Users/guoyafei/Documents/01_个人项目/05_FuGWAS/06_单倍型分析0419/02_haplo/geneD_haplo.txt");
